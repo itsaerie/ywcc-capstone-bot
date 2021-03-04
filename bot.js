930482e9336522e60d9740546849798976886301
Simon Lam <sl755@njit.edu>
2021-03-03 21:58:09 -0500

    Migrating confirmation listener
    *Needs a lot of pretty-ing up and to be shoved into its own listener

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ client.commands = new Discord.Collection();
 
 const { admin_id, prefix, token } = require('./config.json');
 
-
 // command file imports
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -18,11 +17,15 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+const commitChannel = "801473931030036537"
+const execRole = "801473930174660638"
+const confirmChannel = "807160844236095498"
+
 // Listen for messages
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
     if (Object.values(admin_id).includes(message.author.id)) {
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+
         let args = message.content.slice(prefix.length).trim().split(' ');
         const command = args.shift().toLowerCase();
         args = args.join(' ').split(',');
@@ -33,6 +36,35 @@ client.on('message', message => {
             console.error(error);
             message.reply('there was an error trying to execute that command');
         }
+    }
+    if (message.channel == commitChannel && message.mentions.roles.find(role => role.id === execRole) != undefined) { // check if channel sent in is team commitments
+        roles = message.mentions.roles.filter(role => role.id != execRole);
+        // this should be the role which represents the project which a student is interested in.
+        let mentionedRole = roles.first()
+        // send message
+        let replyChannel = message.guild.channels.cache.find(channel => channel.id === confirmChannel)
+        replyChannel.send(`${message.author.toString()} is trying to apply for ${mentionedRole}. Hit ✅ to approve, or hit ❌ to deny.`)
+            .then(reMessage => {
+                const filter = (reaction, user) => {
+                    return ['✅', '❌'].includes(reaction.emoji.name) && user.id !== reMessage.author.id;
+                }
+                reMessage.react('✅')
+                    .finally(() => reMessage.react('❌'))
+                reMessage.awaitReactions(filter, { max: 1, time: 3600000, errors: ['time'] })
+                    .then(collected => {
+                        const reaction = collected.first();
+
+                        if (reaction.emoji.name === '✅') {
+                            message.reply(`You have been confirmed by ${mentionedRole}!`)
+                        } else {
+                            message.reply(`You have been rejected by ${mentionedRole}.`)
+                        }
+                    })
+                    .catch(collected => {
+                        console.log(`only ${collected.size} replies`)
+                    })
+            })
+            .catch(console.error);
     }
 });
 
