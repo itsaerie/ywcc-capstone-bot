5080cf41a86a7f85f019d96054e86cf1a865740f
Simon Lam <sl755@njit.edu>
2021-04-14 00:29:31 -0400

    Create purgecategory.js

---
+++ commands/purgecategory.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: 'purgecategory',
+    description: 'Purge a category and all of its sub-channels',
+    execute(message, args) {
+        const guildid = message.guild.id;
+        var server = message.guild;
+        var channels = server.channels;
+
+        var categoryName = args[0];
+        var parentID = null;
+        channels.cache.forEach(category => {
+            if(category.type == 'category' && category.name.toLowerCase() == categoryName.toLowerCase()) {
+                parentID = category.id
+                channels.cache.forEach(channel => {
+                    if(channel.parentID == parentID) {
+                        console.log(channel.name);
+                        channel.delete()
+                            .then(console.log)
+                            .catch(console.error);
+                    }
+                })
+                category.delete()
+                    .then(console.log)
+                    .catch(console.error);
+            }
+        })
+
+        message.channel.send('done')
+    },
+};
